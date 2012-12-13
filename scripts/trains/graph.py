0ac931dd6fbb589d4ffae3450531d67692e1408b
felipe <felipe@felipe-PC.(none)>
2012-12-12 19:39:22 -0500

    Added style file, and created module for trains

---
+++ scripts/trains/graph.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+
+"""
+
+class Graph:
+
+    """
+    This class handles the mathematical aspect of the network.
+    """
+
+    def __init__(self, nodes, edges, costs=None):
