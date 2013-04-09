a01538fbddee37b95de5d28caa855a0eabf5d0c3
Felipe <user@server>
2013-04-09 17:48:54 -0400

    advanced cars2; only missing lambdas and weights. revamped graphmaker to store lengths, better interface

--- src/cars2.py
+++ src/cars2.py
@@ -1,28 +1,137 @@
 
+import networkx as nx
+import bisect
+import collections
+import random
+
 def guido_to_nx(graph):
+    """
+    Convert a simple python grah into a networkx Digraph.
+
+    :param graph: a mapping of the form ``node: [(target, capacity)]``,
+                  where ``(node, target)`` is an edge in the graph with
+                  capacity ``capacity``.
+    :returns: :class:`networkx.Digraph`
+    """
     G = nx.Digraph()
     for node in graph:
         for tgt, cap in graph[node]:
             G.add_edge(node, tgt, capacity=cap)
     return G
 
+def weighted_choice(choices):
+    """
+    Select an element from a finite discrete distribution.
+
+    :param choices: An iterable of ``(element, weight)`` pairs.
+
+    Note---weights are automatically normalized.
+
+    Taken from http://stackoverflow.com/a/4322940
+    """
+    values, weights = zip(*choices)
+    total = 0
+    cum_weights = []
+    for w in weights:
+        total += w
+        cum_weights.append(total)
+    x = random.random() * total
+    i = bisect.bisect(cum_weights, x)
+    return values[i]
+
 class Simulation(object):
 
-    def __init__(self, graph, arrival_params, weight_fns):
-        self.graph = guido_to_nx(graph)
-        self.exits = 0
-        self.arrivals = set()
+    """
+    This is the main object of the simulation.
+
+    :param graph: A :class:`networkx.Digraph` with edge attributes
+                  ``capacity`` stating the total capacity of the edge. In
+                  this graph, nodes should represent parking lots and
+                  intersections, and edges should represent one-way streets
+                  connecting the nodes. Nodes considered outside the system
+                  should have an attribute 'out' evaluating to True.
+    :param arrival_params: A mapping :math:`\{\mathrm{node}:
+                           (\mu, \sigma, n)\}`, where ``node`` is a node in
+                           the graph, :math:`\mu, \sigma` are the parameters
+                           of the gaussian arrival distribution,
+                           and :math:`n` is the number of cars parked at that
+                           node.
+    """
+
+    def __init__(self, graph, arrival_params):
+        #: Contains all the simulation state in its edge attributes,
+        #: ``weight``, ``capacity``, ``queue``
+        self.graph = graph
+        #: The target node for all vehicles
+        self.exit_node = 0
+        while self.graph.has_node(self.exit_node):
+            self.exit_node += 1
+        outnodes = [node for node, data in self.graph.nodes_iter(data=True)
+                    if data.get('out', False)]
+        for n1, n2, d in self.graph.in_edges_iter(outnodes, data=True):
+            self.graph.remove_edge(n1, n2)
+            self.graph.add_edge(n1, self.exit_node, d)
+
+        arrivals = set()
         for node, p in arrival_params.items():
-            self.arrivals.update((random.gauss(p[0], p[1]), node)
-                                 for _ in xrange(p[2]))
-        self.weight_fns = weight_fns
-        self.paths = 
+            arrivals.update((random.gauss(p[0], p[1]), node)
+                            for _ in xrange(p[2]))
+        self.arrivals = collections.deque(sorted(arrivals))
+        self.__init_num = len(self.arrivals)
+
+        self.exits = []
+        self.time = 0
+        self.path = {}
+
+    @property
+    def in_system(self):
+        """How many cars are in the system."""
+        return self.__init_num - len(self.exits)
 
     def update_weights(self):
+        """
+        Updates the cost of each edge based on the current flow distribution.
+        """
         for s, t in self.graph.edges_iter():
-            self.graph[s][t]['weight'] = self.weight_fns[(s, t)]()
-            #FIXME
+            self.graph[s][t]['weight'] = NotImplemented # FIXME
 
-    def calc_lambda(self):
+    def get_edge_lambdas(self):
+        """Calculate the overall lambda for all intersections."""
+        edges = []
         for s, t, d in self.graph.edges_iter(data=True):
-            
+            d2 = self.graph[t][self.path[t]]
+            if d['queue'] and d2['capacity'] > len(d2['queue']):
+                edges.append((s, t), .1)  # 10 vehicles per minute ~ 600 VPH
+                # FIXME
+        return edges
+
+    def next_event(self):
+        edges, lambdas = zip(*self.get_edge_lambdas())
+        dt = random.expovariate(sum(lambdas))
+        if (not self.arrivals) or dt <= self.arrivals[0]:
+            n1, node = weighted_choice(zip(edges, lambdas))  # a random edge
+            car = self.graph[n1][node]['queue'].popleft()
+            self.time += dt
+        else:
+            self.time, node = self.arrivals.popleft()
+            car = []
+        car.append((node, self.time))
+        try:
+            dest = self.path[node]
+        except KeyError:  # node is self.exit_node
+            self.exits.append(car)
+        else:
+            self.graph[node][dest]['queue'].append(car)
+
+    def calc_path(self):
+        """Calculate the shortest path to exist from every node."""
+        rev = self.graph.reverse(copy=True)
+        paths = rev.single_source_dijkstra_path(self.exit_node)
+        paths.pop(self.exit_node)
+        self.path = {n: p[-2] for n, p in paths.items()}
+
+    def run(self):
+        while self.in_system:
+            self.update_weights()
+            self.calc_path()
+            self.next_event()
--- src/parking/graphmaker.py
+++ src/parking/graphmaker.py
@@ -15,142 +15,254 @@ nodes.
 1.0: Basic Functionality: click & dump
 """
 
+from PIL import Image, ImageDraw
 import Tkinter
 import ImageTk
-import tkMessageBox
+import tkFileDialog
 
-class App(object):
+class Graph(object):
 
-    def __init__(self, backgroundfile):
+    def __init__(self, directed=True):
+        self.graph = {}
+        self._is_directed = directed
+
+    def create_node(self, node):
+        """
+        Creates a new node.
+
+        :param node: Any hashable object
+        """
+        if node in self.graph:
+            raise KeyError("%r is already in graph!" % node)
+        self.graph[node] = {}
+
+    def create_edge(self, node1, node2, length):
+        """
+        Creates a new edge.
+
+        :param node1: first node of the edge.
+        :param node2: second node of the edge.
+        :param length: length or cost of the edge.
+        """
+        if node2 in self.graph[node1]:
+            raise ValueError("%r is already in graph" % tuple(node1, node2))
+        self.graph[node1][node2] = length
+        if not self.is_directed():
+            self.graph[node2][node1] = length
+
+    def is_directed(self):
+        """
+        Whether the graph is directed or not.
+        """
+        return self._is_directed
+
+    def dump(self, graphfilename):
+        """Write the graph to a file."""
+        nodes = list(self.graph)
+        with open(graphfilename, 'wt') as f:
+            f.write(',' + ','.join('"%s"' % str(n) for n in nodes))
+            f.write('\n')
+            for node in nodes:
+                f.write('"%s",' % str(node))
+                f.write(','.join(format(self.graph[node].get(n2, -1), ".3f")
+                                 for n2 in nodes))
+                f.write('\n')
+
+
+class TkGraph(Graph):
+
+    NODE_RADIUS = 5
+
+    def __init__(self, canvas, **kw):
+        self.canvas = canvas
+        super(TkGraph, self).__init__(**kw)
+
+    def create_node(self, node):
+        """
+        Create a node.
+
+        :param node: a tuple ``(x, y)`` of canvas coordinates.
+        """
+        super(TkGraph, self).create_node(node)
+        x, y = node
+        r = TkGraph.NODE_RADIUS
+        node = self.canvas.create_oval(
+            x - r, y - r, x + r, y + r,
+            width=1, fill="black", outline="white")
+
+    def create_edge(self, node1, node2, length):
+        """
+        Create an edge.
+
+        :param node1: tuple of canvas coordinates ``(x1, y1)`` for first node
+        :param node2: tuple of canvas coordinates ``(x2, y2)`` for second node
+        """
+        super(TkGraph, self).create_edge(node1, node2, length, tag='node')
+        if not self.is_directed():
+            arrow = 'both'
+        else:
+            arrow = 'last'
+        x1, y1 = node1
+        x2, y2 = node2
+        self.canvas.create_line((x1, y1, x2, y2),
+                                arrow=arrow,
+                                fill="red",
+                                tag='edge')
+
+class PILGraph(Graph):
+
+    NODE_RADIUS = 5
+
+    def __init__(self, image, **kw):
+        self.image = image
+        self.draw = ImageDraw.Draw(image)
+        super(PILGraph, self).__init__(**kw)
+
+    def create_node(self, node):
+        """
+        Create a node.
+
+        :param node: a tuple ``(x, y)`` of image coordinates.
+        """
+        super(PILGraph, self).create_node(node)
+        x, y = node
+        r = PILGraph.NODE_RADIUS
+        self.draw.ellipse(
+            (x - r, y - r, x + r, y + r),
+            fill='black', outline='white')
+
+    def create_edge(self, node1, node2, length):
+        """
+        Create an edge.
+
+        :param node1: tuple of image coordinates ``(x1, y1)`` for first node
+        :param node2: tuple of image coordinates ``(x2, y2)`` for second node
+        """
+        super(PILGraph, self).create_edge(node1, node2, length)
+        x1, y1 = node1
+        x2, y2 = node2
+        if self.is_directed():
+            arrow = 'last'
+        else:
+            arrow = 'both'
+        self.draw_arrow((x1, y1, x2, y2), arrow=arrow, fill='yellow')
+
+    def draw_arrow(self, xy, arrow='both', fill='black', width=1):
+        """
+        Draw an arrow between the two points.
+
+        :param arrow: One of ``'first'``, ``'last'``, ``'both'``
+        """
+        import math
+        def rotate(x, y, theta):
+            return (math.cos(theta) * x - math.sin(theta) * y,
+                    math.sin(theta) * x - math.sin(theta) * x)
+        def tri(x, y, theta, scale=1):
+            pts = [(0, 2), (2, 0), (0, -2)]
+            pts = [rotate(i, j, theta) for i, j in pts]
+            return [(x + i * scale, y + j * scale) for i, j in pts]
+
+        self.draw.line(xy, fill, width)
+        x1, y1, x2, y2 = xy
+        theta = math.atan2(y2 - y1, x2 - x1)
+        if arrow in {'both', 'last'}:
+            self.draw.polygon(tri(x2, y2, theta, width), fill=fill)
+        if arrow in {'both', 'first'}:
+            self.draw.polygon(tri(x1, y1, math.pi + theta, width), fill=fill)
+
+    def dump(self, imagefilename):
+        """Save the image to a file."""
+        self.image.save(imagefilename)
+
+
+class ImageGraph(PILGraph, TkGraph):
+
+    def __init__(self):
         self.root = Tkinter.Tk()
         self.root.title("graphmaker")
-        frame = Tkinter.Frame(self.root)
-        frame.pack()
-        self.root.bind("d", self.focus_entry)
-        self.root.bind("e", self.button_click)
-        self.root.bind("n", self.button_click)
-        self.root.bind("<4>", self.roll_wheel)
-        self.root.bind("<5>", self.roll_wheel)
-        self.button = Tkinter.Button(frame, text="Node", width=50,
-                                     command=self.button_click)
-        self.button.pack()
-        self.entry = Tkinter.Entry(frame)
-        self.entry.pack(fill=Tkinter.X)
-        self.entry.bind("<FocusIn>", self.clear_entry)
-        self.entry.bind("<FocusOut>", self.leave_entry)
-        self.entry.insert(0, "Node Name")
 
-        self.vscroll = Tkinter.Scrollbar(frame)
-        self.vscroll.pack(side=Tkinter.RIGHT, fill=Tkinter.Y)
-        self.hscroll = Tkinter.Scrollbar(frame)
-        self.hscroll.pack(side=Tkinter.BOTTOM, fill=Tkinter.X)
-        self.canvas = Tkinter.Canvas(frame, bg="white",
-                                     width=800, height=650,
-                                     yscrollcommand=self.vscroll.set,
-                                     xscrollcommand=self.hscroll.set,
-                                     scrollregion=(0,0,741, 766))
-        self.imsize = (741.0, 766.0)
-        self.canvas.pack()
-        self.canvas.bind("<Button-1>", self.mouse_click)
-        self.vscroll.config(command=self.canvas.yview)
-        self.hscroll.config(command=self.canvas.xview, orient=Tkinter.HORIZONTAL)
-        self.photoimage = ImageTk.PhotoImage(file=backgroundfile)
-        self.image = self.canvas.create_image(0, 0, image=self.photoimage,
-                                              anchor="nw")
+        message = "Select background image"
+        bgfilename = tkFileDialog.askopenfilename(parent=self.root,
+                                                          title=message)
+        im = Image.open(bgfilename)
+        width, height = im.size
+        cwidth = min(width, self.root.winfo_screenwidth() - 200)
+        cheight = min(height, self.root.winfo_screenheight() - 200)
+        self.root.minsize(cwidth, cheight)
 
-        self.mode = "NODE"
-        self.nodes = {}
-        self.startnode = None
-        self.names = {}
+        self.dir_var = Tkinter.IntVar()
+        self.make_widgets(width, height, cwidth, cheight, bgfilename)
+
+        self.startnode_id = None
+        self.scrollmode = False
+
+        super(ImageGraph, self).__init__(image=im,
+                                         canvas=self.canvas,
+                                         directed=True)
 
     def start(self):
         self.root.mainloop()
 
-    def button_click(self):
-        if self.mode == "NODE":
-            self.mode = "EDGE"
-            self.button.config(text="Edge")
+    def create_edge_handle(self, node_id):
+        """Process a click to create an edge."""
+        if self.startnode_id is None:
+            self.startnode_id = node_id
+            self.canvas.itemconfigure(node_id, fill="red")
+            return
+        # If we already have a startnode, we need to make an edge
+        startnode = self.get_node_center(self.startnode_id)
+        node = self.get_node_center(node_id)
+        length = self.segment_length(startnode, node)
+        try:
+            self.create_edge(startnode, node, length)
+        except ValueError:
+            pass
         else:
-            self.mode = 'NODE'
-            self.button.config(text="Node")
+            line = self.canvas.find_all()[-1]  # NOT THREAD SAFE!
+            self.root.after(500,
+                    lambda: self.canvas.itemconfigure(line, fill="yellow"))
+        self.canvas.itemconfigure(self.startnode_id, fill='black')
+        self.startnode_id = None
 
-    def clear_entry(self, event):
-        if self.entry.get() == "Node Name":
-            self.entry.delete(0, Tkinter.END)
+    @staticmethod
+    def segment_length(n1, n2):
+        """Compute the length of a segment."""
+        x1, y1 = n1
+        x2, y2 = n2
+        return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** .5
 
-    def leave_entry(self, event):
-        if not self.entry.get():
-            self.entry.insert(0, "Node Name")
+    def get_node_center(self, node_id):
+        """Computes the center of a node."""
+        x1, y1, x2, y2 = self.canvas.coords(node_id)
+        return int(.5 * (x1 + x2)), int(.5 * (y1 + y2))
 
-    def focus_entry(self, event):
-        self.entry.focus_set()
+    def is_directed(self):
+        """Whether the graph is directed or not."""
+        return self.dir_var.get()
+
+    def select_items(self, x, y, tag):
+        """Select the items close to (x ,y) with tag."""
+        nodes = set(self.canvas.find_overlapping(x-5, y-5, x+5, y+5))
+        nodes.remove(self.bg)
+        if tag is not None:
+            nodes.intersection_update(self.canvas.find_withtag(tag))
+        return nodes
 
     def mouse_click(self, event):
-        x = self.canvas.canvasx(event.x) + self.hscroll.get()[0] * self.imsize[0]
-        y = self.canvas.canvasx(event.y) + self.vscroll.get()[0] * self.imsize[1]
-        nodes = list(self.canvas.find_overlapping(x-5, y-5, x+5, y+5))
-        nodes.remove(self.image)
-        if self.mode == "NODE":
-            name = self.entry.get()
-            if not name:
-                tkMessageBox.showerror("Invalid Name!",
-                                       "Please enter a node name")
-                return
-            elif name in self.names.values():
-
-                return
-            elif nodes:
-                tkMessageBox.showerror("Node Exists!",
-                                       "Too close to an existing node!")
-                return
-            node = self.canvas.create_oval(x-5, y-5, x+5, y+5, width=1,
-                                    fill="black", outline="white",
-                                    tags=(name,))
-            tid= self.canvas.create_text(x+10, y, anchor='w')
-            self.canvas.insert(tid, 0, name)
-            self.entry.delete(0, Tkinter.END)
-            if self.root.focus_get() is not self.entry:
-                self.entry.insert(0, "Node Name")
-            self.nodes[node] = []
-            self.names[node] = name
-        elif self.mode == "EDGE" and nodes:
-            def dist(node):
-                x1, y1, x2, y2 = self.canvas.coords(node)
-                xx = .5 * (x1 + x2)
-                yy = .5 * (y1 + y2)
-                return ((xx - x) ** 2 + (yy - y) ** 2) ** .5
-            node = min(nodes, key=dist)
-            if self.startnode is None:
-                self.startnode = node
-                self.canvas.itemconfigure(node, fill="red")
-                return
-            elif node not in self.nodes[self.startnode]:
-                x1, y1, x2, y2 = self.canvas.coords(self.startnode)
-                xo = .5 * (x1 + x2)
-                yo = .5 * (y1 + y2)
-                x1, y1, x2, y2 = self.canvas.coords(node)
-                xf = .5 * (x1 + x2)
-                yf = .5 * (y1 + y2)
-                line = self.canvas.create_line((xo, yo, xf, yf),
-                                               fill="yellow")
-                self.root.after(500, lambda: self.canvas.itemconfigure(
-                    line, fill="gray"))
-            self.canvas.itemconfigure(self.startnode, fill="black")
-            self.nodes[self.startnode].append(node)
-            self.nodes[node].append(self.startnode)
-            self.startnode = None
-
-    def dump(self, outfile):
-        names2nodes = dict((name, node) for node, name in self.names.items())
-        names = sorted(self.names.items())
-        with open(outfile, 'wt') as f:
-            f.write(',' + ','.join(zip(*names)[0]))
-            for name, node in names:
-                f.write(name + ',')
-                for _, node2 in names:
-                    f.write(node2 in self.nodes[node])
-                    f.write(',')
-                f.write('\n')
+        x = self.canvas.canvasx(event.x)
+        y = self.canvas.canvasy(event.y)
+        nodes = self.select_items(x, y, 'node')
+        if not nodes and not self.startnode_id:  # Clicked on a new area
+            self.create_node((x, y))
+        elif nodes:
+            def dist(node_id):
+                xx, yy = self.get_node_center(node_id)
+                return self.segment_length((xx, yy), (x, y))
+            node_id = min(nodes, key=dist)
+            self.create_edge_handle(node_id)
+        else: # cancel selection
+            self.canvas.itemconfigure(self.startnode_id, fill='black')
+            self.startnode_id = None
 
     def roll_wheel(self, event):
         if event.num == 4:
@@ -161,3 +273,54 @@ class App(object):
             self.canvas.xview('scroll', 1, 'units')
         elif event.num == 7:
             self.canvas.xview('scroll', -1, 'units')
+
+    def save(self):
+        """Save the image and graph files."""
+        message = "Save the graph CSV file"
+        graphname = tkFileDialog.asksaveasfilename(parent=self.root,
+                                                   title=message)
+        if graphname:
+            Graph.dump(self, graphname)
+        message = "Save the image file"
+        imagename = tkFileDialog.asksaveasfilename(parent=self.root,
+                                                   title=message)
+        if imagename:
+            PILGraph.dump(self, imagename)
+
+    def make_widgets(self, width, height, cwidth, cheight, bg):
+        """Make the necessary widgets and bindings."""
+        self.dir_var.set(1)
+        frame = Tkinter.Frame(self.root)
+        frame.pack(side=Tkinter.TOP)
+        Tkinter.Checkbutton(frame,
+                            text="Directed Arcs?",
+                            variable=self.dir_var).pack(side=Tkinter.LEFT)
+        Tkinter.Button(frame, text="Save to File", command=self.save).pack()
+        frame = Tkinter.Frame(self.root)
+        frame.pack()
+        self.vscroll = Tkinter.Scrollbar(frame)
+        self.vscroll.pack(side=Tkinter.RIGHT, fill=Tkinter.Y)
+        self.hscroll = Tkinter.Scrollbar(frame)
+        self.hscroll.pack(side=Tkinter.BOTTOM, fill=Tkinter.X)
+        self.canvas = Tkinter.Canvas(frame,
+                                     bg="white",
+                                     yscrollcommand=self.vscroll.set,
+                                     xscrollcommand=self.hscroll.set)
+        self.canvas.pack()
+        self.canvas.bind("<Button-1>", self.mouse_click)
+        self.vscroll.config(command=self.canvas.yview)
+        self.hscroll.config(command=self.canvas.xview,
+                            orient=Tkinter.HORIZONTAL)
+        self.photoimage = ImageTk.PhotoImage(file=bg)
+        self.bg = self.canvas.create_image(0, 0, image=self.photoimage,
+                                              anchor="nw")
+        self.canvas.config(height=cheight,
+                           width=cwidth,
+                           scrollregion=(0,0,width, height))
+        self.root.bind("<4>", self.roll_wheel)
+        self.root.bind("<5>", self.roll_wheel)
+
+
+if __name__ == "__main__":
+    app = ImageGraph()
+    app.start()
