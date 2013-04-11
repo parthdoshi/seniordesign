ca30fa87125c65923a95562cc9b853407f87152e
Felipe <user@server>
2013-04-11 00:42:01 -0400

    added most of the nodes and edges, working on removing erroneous edges

---
+++ src/parking/dragbox.py
@@ -0,0 +1,101 @@
+
+"""
+Author: Felipe Ochoa
+Date: 9 April 2012
+Name: Tkinter Drag Box
+
+An object to create simple drag boxes on a Tkinter Canvas.
+"""
+
+class DragBox(object):
+
+    """
+    A container object to implement a dragbox on a Tkinter Canvas.
+
+    The only parameter it takes is the canvas object, which is the
+    canvas on which to set up the box.
+
+    It provides three hooks with its methods ``click_hook``,
+    ``drag_hook``, and ``release_hook``. Each of those methods is called
+    after  handling its respective event with a single positional
+    parameter `coords`, which is a tuple ``(x1, y1, x2, y2)`` of the
+    box's coordinates at that point.
+
+    The default action for each method is to pass, so they may safely be
+    overriden with direct assignment. I.e.,
+
+        db = DragBox(canvas)
+        db.release_hook = my_function
+
+    where ``my_function`` takes a single parameter.
+
+    ``DragBox`` objects have a boolean attribute ``active`` to
+    determine whether it is operating or not.
+    """
+
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.mouse_x = 0
+        self.mouse_y = 0
+        self.box = canvas.create_rectangle(0, 0, 0, 0)
+        canvas.itemconfig(self.box,
+                          dash=(5,5),
+                          fill='',
+                          state='hidden',
+                          outline='gray')
+        self.active = True
+        self.rebind()
+
+    def coords(self, event):
+        """Convert event coordinates into canvas coordinates."""
+        return self.canvas.canvasx(event.x), self.canvas.canvasy(event.y)
+
+    def rebind(self):
+        """Used to reestablish the event bindings."""
+        self.canvas.bind("<Button-1>", self.mouse_click, add='+')
+        self.canvas.bind("<ButtonRelease-1>", self.mouse_up, add='+')
+        self.canvas.bind("<B1-Motion>", self.mouse_drag, add='+')
+
+    def mouse_click(self, event):
+        if self.active:
+            self.canvas._root().config(cursor='tcross')
+            self.mouse_x, self.mouse_y = self.coords(event)
+            self.canvas.coords(self.box, self.mouse_x, self.mouse_y,
+                               self.mouse_x, self.mouse_y)
+            self.canvas.itemconfig(self.box, state='normal')
+            self.click_hook((self.mouse_x, self.mouse_y) * 2)
+
+    def mouse_drag(self, event):
+        if self.active:
+            x, y = self.coords(event)
+            self.canvas.coords(self.box, self.mouse_x, self.mouse_y, x, y)
+            self.drag_hook((self.mouse_x, self.mouse_y, x, y))
+
+    def mouse_up(self, event):
+        if self.active:
+            self.canvas._root().config(cursor='arrow')
+            self.canvas.itemconfig(self.box, state='hidden')
+            self.release_hook(tuple(self.canvas.coords(self.box)))
+
+    def click_hook(self, coords):
+        """Hook, called after handling mouse click event."""
+        pass
+
+    def drag_hook(self, coords):
+        """Hook, called after handling the drag event."""
+        pass
+
+    def release_hook(self, coords):
+        """Hook, called after handling the mouse release event."""
+        pass
+
+
+if __name__ == "__main__":
+    import Tkinter
+    root = Tkinter.Tk()
+    canvas = Tkinter.Canvas(root, width=200, height=200, bg='white')
+    canvas.pack()
+    d = DragBox(canvas)
+    def f(c): print c
+    d.release_hook = f
+    root.mainloop()
--- src/parking/graphmaker.py
+++ src/parking/graphmaker.py
@@ -19,6 +19,8 @@ from PIL import Image, ImageDraw
 import Tkinter
 import ImageTk
 import tkFileDialog
+import dragbox
+import csv
 
 class Graph(object):
 
@@ -33,7 +35,7 @@ class Graph(object):
         :param node: Any hashable object
         """
         if node in self.graph:
-            raise KeyError("%r is already in graph!" % node)
+            raise KeyError("%s is already in graph!" % str(node))
         self.graph[node] = {}
 
     def create_edge(self, node1, node2, length):
@@ -50,11 +52,30 @@ class Graph(object):
         if not self.is_directed():
             self.graph[node2][node1] = length
 
+    def delete_edge(self, edge):
+        """
+        Removes an edge from the graph.
+
+        :param edge: An iterable of the form ``(node1, node2)``
+        """
+        node1, node2 = edge
+        del self.graph[node1][node2]
+        if self.is_directed():
+            del self.graph[node2][node1]
+
     def is_directed(self):
         """
         Whether the graph is directed or not.
         """
         return self._is_directed
+
+    def load(self, graph):
+        """
+        Replace the current graph with another one.
+
+        No checking is done on graph -- it is assumed to be well formed.
+        """
+        self.graph = graph
 
     def dump(self, graphfilename):
         """Write the graph to a file."""
@@ -71,7 +92,7 @@ class Graph(object):
 
 class TkGraph(Graph):
 
-    NODE_RADIUS = 5
+    NODE_RADIUS = 10
 
     def __init__(self, canvas, **kw):
         self.canvas = canvas
@@ -84,11 +105,15 @@ class TkGraph(Graph):
         :param node: a tuple ``(x, y)`` of canvas coordinates.
         """
         super(TkGraph, self).create_node(node)
+        self.paint_node(node)
+
+    def paint_node(self, node):
+        """Draw the node on the canvas."""
         x, y = node
         r = TkGraph.NODE_RADIUS
-        node = self.canvas.create_oval(
+        self.canvas.create_oval(
             x - r, y - r, x + r, y + r,
-            width=1, fill="black", outline="white")
+            width=1, fill="black", outline="white", tag='node')
 
     def create_edge(self, node1, node2, length):
         """
@@ -97,7 +122,11 @@ class TkGraph(Graph):
         :param node1: tuple of canvas coordinates ``(x1, y1)`` for first node
         :param node2: tuple of canvas coordinates ``(x2, y2)`` for second node
         """
-        super(TkGraph, self).create_edge(node1, node2, length, tag='node')
+        super(TkGraph, self).create_edge(node1, node2, length)
+        self.pain_edge(node1, node2)
+
+    def paint_edge(self, node1, node2):
+        """Draw an edge on the canvas."""
         if not self.is_directed():
             arrow = 'both'
         else:
@@ -108,6 +137,22 @@ class TkGraph(Graph):
                                 arrow=arrow,
                                 fill="red",
                                 tag='edge')
+
+    def load(self, graph):
+        """
+        Replace the current graph.
+
+        Nodes must be tuples of their coordinates.
+        """
+        super(TkGraph, self).load(graph)
+        self.canvas.delete('node')
+        self.canvas.delete('edge')
+        for node in graph:
+            self.paint_node(node)
+        for node in graph:
+            for dest in graph[node]:
+                self.paint_line(node, dest)
+
 
 class PILGraph(Graph):
 
@@ -184,45 +229,22 @@ class ImageGraph(PILGraph, TkGraph):
         message = "Select background image"
         bgfilename = tkFileDialog.askopenfilename(parent=self.root,
                                                           title=message)
+        if not bgfilename:
+            raise ValueError("Invalid background filename")
         im = Image.open(bgfilename)
         width, height = im.size
-        cwidth = min(width, self.root.winfo_screenwidth() - 200)
+        cwidth = min(width, self.root.winfo_screenwidth())
         cheight = min(height, self.root.winfo_screenheight() - 200)
+        self.root.geometry("%dx%d" % (cwidth, cheight))
         self.root.minsize(cwidth, cheight)
-
         self.dir_var = Tkinter.IntVar()
         self.make_widgets(width, height, cwidth, cheight, bgfilename)
-
-        self.startnode_id = None
-        self.scrollmode = False
-
         super(ImageGraph, self).__init__(image=im,
                                          canvas=self.canvas,
                                          directed=True)
 
     def start(self):
         self.root.mainloop()
-
-    def create_edge_handle(self, node_id):
-        """Process a click to create an edge."""
-        if self.startnode_id is None:
-            self.startnode_id = node_id
-            self.canvas.itemconfigure(node_id, fill="red")
-            return
-        # If we already have a startnode, we need to make an edge
-        startnode = self.get_node_center(self.startnode_id)
-        node = self.get_node_center(node_id)
-        length = self.segment_length(startnode, node)
-        try:
-            self.create_edge(startnode, node, length)
-        except ValueError:
-            pass
-        else:
-            line = self.canvas.find_all()[-1]  # NOT THREAD SAFE!
-            self.root.after(500,
-                    lambda: self.canvas.itemconfigure(line, fill="yellow"))
-        self.canvas.itemconfigure(self.startnode_id, fill='black')
-        self.startnode_id = None
 
     @staticmethod
     def segment_length(n1, n2):
@@ -247,22 +269,6 @@ class ImageGraph(PILGraph, TkGraph):
         if tag is not None:
             nodes.intersection_update(self.canvas.find_withtag(tag))
         return nodes
-
-    def mouse_click(self, event):
-        x = self.canvas.canvasx(event.x)
-        y = self.canvas.canvasy(event.y)
-        nodes = self.select_items(x, y, 'node')
-        if not nodes and not self.startnode_id:  # Clicked on a new area
-            self.create_node((x, y))
-        elif nodes:
-            def dist(node_id):
-                xx, yy = self.get_node_center(node_id)
-                return self.segment_length((xx, yy), (x, y))
-            node_id = min(nodes, key=dist)
-            self.create_edge_handle(node_id)
-        else: # cancel selection
-            self.canvas.itemconfigure(self.startnode_id, fill='black')
-            self.startnode_id = None
 
     def roll_wheel(self, event):
         if event.num == 4:
@@ -297,30 +303,199 @@ class ImageGraph(PILGraph, TkGraph):
                             variable=self.dir_var).pack(side=Tkinter.LEFT)
         Tkinter.Button(frame, text="Save to File", command=self.save).pack()
         frame = Tkinter.Frame(self.root)
-        frame.pack()
+        frame.pack(fill='both', expand=True)
         self.vscroll = Tkinter.Scrollbar(frame)
         self.vscroll.pack(side=Tkinter.RIGHT, fill=Tkinter.Y)
         self.hscroll = Tkinter.Scrollbar(frame)
         self.hscroll.pack(side=Tkinter.BOTTOM, fill=Tkinter.X)
         self.canvas = Tkinter.Canvas(frame,
                                      bg="white",
+                                     scrollregion=(0,0,width, height),
                                      yscrollcommand=self.vscroll.set,
                                      xscrollcommand=self.hscroll.set)
-        self.canvas.pack()
-        self.canvas.bind("<Button-1>", self.mouse_click)
+        self.canvas.pack(fill='both', expand=True)
         self.vscroll.config(command=self.canvas.yview)
         self.hscroll.config(command=self.canvas.xview,
                             orient=Tkinter.HORIZONTAL)
         self.photoimage = ImageTk.PhotoImage(file=bg)
         self.bg = self.canvas.create_image(0, 0, image=self.photoimage,
                                               anchor="nw")
-        self.canvas.config(height=cheight,
-                           width=cwidth,
-                           scrollregion=(0,0,width, height))
         self.root.bind("<4>", self.roll_wheel)
         self.root.bind("<5>", self.roll_wheel)
 
 
+class NodeMaker(ImageGraph):
+
+    def __init__(self):
+        super(NodeMaker, self).__init__()
+        self.startnode_id = None
+        self.canvas.bind("<Button-1>", self.mouse_click)
+
+
+    def create_edge_handle(self, node_id):
+        """Process a click to create an edge."""
+        if self.startnode_id is None:
+            self.startnode_id = node_id
+            self.canvas.itemconfigure(node_id, fill="red")
+            return
+        # If we already have a startnode, we need to make an edge
+        startnode = self.get_node_center(self.startnode_id)
+        node = self.get_node_center(node_id)
+        length = self.segment_length(startnode, node)
+        try:
+            self.create_edge(startnode, node, length)
+        except ValueError:
+            pass
+        else:
+            line = self.canvas.find_all()[-1]  # NOT THREAD SAFE!
+            self.root.after(500,
+                    lambda: self.canvas.itemconfigure(line, fill="yellow"))
+        self.canvas.itemconfigure(self.startnode_id, fill='black')
+        self.startnode_id = None
+
+    def mouse_click(self, event):
+        x = self.canvas.canvasx(event.x)
+        y = self.canvas.canvasy(event.y)
+        nodes = self.select_items(x, y, 'node')
+        if not nodes and not self.startnode_id:  # Clicked on a new area
+            self.create_node((x, y))
+        elif nodes:
+            def dist(node_id):
+                xx, yy = self.get_node_center(node_id)
+                return self.segment_length((xx, yy), (x, y))
+            node_id = min(nodes, key=dist)
+            self.create_edge_handle(node_id)
+        else: # cancel selection
+            self.canvas.itemconfigure(self.startnode_id, fill='black')
+            self.startnode_id = None
+
+class EdgeMaker(ImageGraph):
+
+    def __init__(self, nodefile):
+        super(EdgeMaker, self).__init__()
+        nodes = eval(open(nodefile).read())
+        for node in nodes:
+            self.create_node(node)
+        self.box = dragbox.DragBox(self.canvas)
+        self.box.release_hook = self.select_nodes
+        self.selection = []
+        self.root.bind("r", lambda e: self.rotate_labels())
+        self.root.bind("a", lambda e: self.apply_edges())
+        self.root.bind("d", lambda e: self.deselect())
+        self.root.bind("f", lambda e: self.flip_labels())
+
+    def select_nodes(self, box):
+        additions = set(self.canvas.find_enclosed(*box))
+        additions.difference_update(self.selection)
+        additions.intersection_update(self.canvas.find_withtag('node'))
+        self.selection.extend(additions)
+        for node_id in self.selection:
+            self.canvas.itemconfig(node_id, fill='white')
+            self.canvas.addtag_withtag('selected', node_id)
+        self.label_selection()
+
+    def label_selection(self):
+        self.canvas.delete('text')
+        for i, node_id in enumerate(self.selection):
+            self.label_node(node_id, str(i))
+
+    def label_node(self, node_id, label):
+        x, y = self.get_node_center(node_id)
+        self.canvas.create_text(x, y, text=label, tag='text')
+
+    def rotate_labels(self):
+        self.selection[:] = self.selection[1:] + [self.selection[0]]
+        self.label_selection()
+
+    def flip_labels(self):
+        self.selection.reverse()
+        self.label_selection()
+
+    def apply_edges(self):
+        for base, target in zip(self.selection, self.selection[1:]):
+            n1 = self.get_node_center(base)
+            n2 = self.get_node_center(target)
+            length = self.segment_length(n1, n2)
+            self.create_edge(n1, n2, length)
+        self.deselect()
+
+    def deselect(self):
+        for node_id in self.selection:
+            self.canvas.itemconfig(node_id, fill='black')
+            self.canvas.delete('text')
+            self.canvas.dtag(node_id, 'selected')
+        self.selection = []
+
+
+class EdgePurger(ImageGraph):
+
+    def __init__(self, graphfile):
+        super(EdgePurger, self).__init__()
+        self.load_file(graphfile)
+        self.selected = None
+        self.canvas.bind("<Button-1>", self.mouse_click)
+
+    def load_file(self, graphfile):
+        graph = {}
+        with open(graphfile, 'rb') as csvfile:
+            reader =  csv.reader(csvfile)
+            nodes = map(eval, next(reader)[1:])
+            for line in reader:
+                base = eval(line.pop(0))
+                graph[base] = dict((n1, n2)
+                                   for n1, n2 in zip(nodes, map(float, line))
+                                   if n2 > 0)
+        self.load(graph)
+
+    def remove_edge(self, edge_id):
+        """Remove an edge from the graph."""
+        nodes = self.get_edge_nodes(edge_id)
+        self.delete_edge(nodes)
+
+    def get_edge_nodes(self, edge):
+        """Get the nodes (as tuples) at the edge vertices."""
+        x1, y1, x2, y2 = self.canvas.coords(edge)
+        assert (x1, y1) in self.graph
+        assert (x2, y2) in self.graph
+        return (x1, y1), (x2, y2)
+
+    def mouse_click(self, event):
+        x = self.canvas.canvasx(event.x)
+        y = self.canvas.canvasy(event.y)
+        print "%f, %f" % (x, y)
+        edges = list(self.select_items(x, y, 'edge'))
+        if edges:
+            self.select_edge(edges[0])
+        else:
+            self.deselect()
+        if len(edges) > 1:
+            self.root.bind('r', self.multiple_edges_callback(edges))
+
+    def multiple_edges_callback(self, edges):
+        """Create a callback to rotate the selected edge."""
+        def callback(_):
+            edges[:] = edges[1:] + [edges[0]]
+            self.select_edge(edges[0])
+        return callback
+
+    def select_edge(self, edge_id):
+        """Select an edge."""
+        self.deselect()
+        self.canvas.addtag_withtag('selected', edge_id)
+        self.canvas.itemconfig(edge_id, fill='white', width=3)
+        self.selected = edge_id
+
+    def deselect(self):
+        """Deselect edge."""
+        self.canvas.itemconfig('selected', fill='red', width=1)
+        self.canvas.dtag('selected', 'selected')
+        self.selected = None
+
+
 if __name__ == "__main__":
-    app = ImageGraph()
-    app.start()
+    try:
+        app = EdgePurger('graph.csv')
+    except ValueError:
+        pass
+    else:
+        app.start()
