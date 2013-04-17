f67278a136fe0e754f89b2114d9dfd62acb6f0ab
Felipe <user@server>
2013-04-17 16:59:34 -0400

    fixed a couple of demand generation bugs; refactored graph making function into class

--- src/septa/septa.py
+++ src/septa/septa.py
@@ -4,11 +4,11 @@ from __future__ import division
 import sqlite3
 import pyglpk
 
-LINK_DB = "final.db"
-TABLE_NAME = "links"
-DEMAND_FILE = "demand.csv"
-OUTPUT = "septa-results.csv"
-
+LINK_DB = "final.db"           # Path to database
+TABLE_NAME = "links"           # Name of link table in database
+DEMAND_FILE = "demand.csv"     # Path to demand CSV
+OUTPUT = "septa-results.csv"   # Where to output results
+OVERFLOW = 0                   # Cost of overflow links (0 for infinity)
 
 STADIUM_NODE = 0
 STADIUM_STOPS = {
@@ -26,12 +26,11 @@ GAMETIME = 16 * 60
 
 def main():
     # There's an if __name__ == "__main__": main() at the bottom
-    import cProfile
-    cProfile.run('main_sim(GAMETIME)', 'profile.stats')
+    main_sim(GAMETIME, LINK_DB, OUTPUT)
 
-def main_sim(gametime):
+def main_sim(gametime, dbfilename, outfilename):
     print "Starting"
-    graph = make_guido_graph(LINK_DB)
+    graph = make_guido_graph(dbfilename)
     print "Made the python graph."
     capacity = get_capacity()
     print "Loaded the network's capacity"
@@ -39,7 +38,7 @@ def main_sim(gametime):
     print "Loaded the network's demand"
     flow = run_ook(graph, demand, capacity)
     print "Found the optimal flow, size %d" % len(flow)
-    flow_to_csv(flow, OUTPUT)
+    flow_to_csv(flow, outfilename)
     print "Wrote the results to %s" % OUTPUT
 
 def get_capacity():
@@ -75,122 +74,26 @@ def get_demand(gametime):
     csv = [l.split(',') for l in open(DEMAND_FILE).read().split('\n') if l]
     demand = {}
     for row in csv[1:]:
-        demand[row[0]] = sum(map(int, row[1]))
+        demand[row[0]] = 0
         time = gametime - 80
         for v in map(int, row[1:]):
             val = int(v / 4)
             for _ in xrange(4):
                 time += 5
+                node = (STADIUM_NODE, GraphMaker.minutes_to_str(time))
                 try:
-                    demand[(STADIUM_NODE, minutes_to_str(time))] -= val
+                    demand[node] -= val
                 except KeyError:
-                    demand[(STADIUM_NODE, minutes_to_str(time))] = -val
+                    demand[node] = -val
+                demand[row[0]] += val
     return demand
 
 def make_guido_graph(dbfile):
     """
     Reads in the septa database and outputs a space-time graph.
-
-    Output is a mapping of the form
-
-        {
-          (node-name, time-object):
-                  set({
-                    [(node-name, time-object), cost, type],
-                    ... ,
-                    [(node-name, time-object), cost, type],
-                  })
-        }
-
-    where one node is in the set of another node iff there is a
-    direct link between them, the cost is simply the difference
-    in seconds of their time coordinate, and type refers to the
-    SEPTA API type to distinguish between buses, trolleys, etc.
-    There is the addition of the type 'wait' for waiting links.
     """
-    connection = sqlite3.connect(
-            dbfile,
-            detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
-    cursor = connection.cursor()
-    # Build the initial node list as (origin,departure-time) and
-    # (destination,arrival-time) pairs
-    cursor.execute('SELECT DISTINCT origin, departure '
-                   'FROM %s;' % TABLE_NAME)
-    nodes = map(tuple, cursor.fetchall())
-    cursor.execute('SELECT DISTINCT dest, arrival '
-                   'FROM %s;' % TABLE_NAME)
-    nodes.extend(map(tuple, cursor.fetchall()))
-
-    # Use sorting to insert waiting links of the form
-    #      ('xxx', 12:00:00) -> ('xxx', 12:03:00)
-    # ensuring the links are only among consecutive nodes
-    nodes = sorted(set(nodes))
-    graph = {}
-    stadium_nodes = []
-    print "Node count: %d, starting to make edges" % len(nodes)
-    for i, node in enumerate(nodes):
-        cursor.execute('SELECT dest, arrival, duration, type ' +
-                       'FROM %s ' % TABLE_NAME +
-                       'WHERE origin=? AND departure=?;',
-                       node)
-        try:
-            dests, arrs, costs, types = zip(*cursor.fetchall())
-        except ValueError: # no values
-            graph[node] = set()
-            continue
-        dapairs = map(tuple, zip(dests, arrs))
-        graph[node] = set(zip(dapairs, costs, types))
-
-        try:  # This entire block is for the waiting links
-            next_node, time = nodes[i+1]
-        except IndexError:
-            pass
-        else:
-            if next_node == node[0]:
-                cost = get_delta_mins(node[1], time)
-                t = 'wait'
-                graph[node].add((nodes[i+1], cost, t))
-
-        # Now we (potentially) add the links to the stadium
-        if node[0] in STADIUM_STOPS:
-            cost = STADIUM_STOPS[node[0]]
-            time = node[1] + cost
-            raise StupidityError
-            graph[node].add(((STADIUM_NODE, minutes_to_str(time)),
-                             cost, 'walk'))
-            stadium_nodes.append((STADIUM_NODE, minutes_to_str(time)))
-
-    # Now add the waiting links for the stadium nodes
-    new_nodes = set((STADIUM_NODE, minutes_to_str(GAMETIME + 5 * i))
-                    for i in xrange(-15, 13)).difference(stadium_nodes)
-    stadium_nodes.extend(new_nodes)
-    stadium_nodes.sort()
-    print "Node count: %d, making stadium nodes" % len(graph)
-    for n1, n2 in zip(stadium_nodes, stadium_nodes[1:]):
-        cost = get_delta_mins(n1[1], n2[1])
-        graph[n1] = set([(n2, cost, 'wait')])
-        print n1
-    try:
-        graph[stadium_nodes[-1]] = set()
-    except IndexError:
-        pass
-    # Now we add the zipcode nodes
-    print "Node count: %d, making zipcode nodes" % len(graph)
-    cursor.execute('SELECT DISTINCT zipcode FROM stops')
-    zipcodes = zip(*cursor.fetchall())[0]
-    for zipcode in zipcodes:
-        cursor.execute('SELECT origin, departure '
-                       'FROM links '
-                       'INNER JOIN stops '
-                       'ON origin=stop_id '
-                       'WHERE stops.zipcode=?;',
-                       (zipcode,))
-        graph[zipcode] = set((tuple(r), 0, 'init')
-                             for r in cursor.fetchall())
-        graph[zipcode].add(((STADIUM_NODE, minutes_to_str(GAMETIME - 75)),
-                            1000, 'debug'))
-    print "Node count: %d, finished making graph" % len(graph)
-    return graph
+    graph_maker = GraphMaker(dbfile, TABLE_NAME, OVERFLOW)
+    return graph_maker.make_graph()
 
 def run_ook(graph, demand, capacity):
     """
@@ -211,29 +114,42 @@ def run_ook(graph, demand, capacity):
     """
     nodes = set(graph.keys())
     for vals in graph.values():
-        nodes.update(t[0] for t in vals)
+        nodes.update(vals.keys())
     num_nodes = len(nodes)
     arcs = sum(map(len, graph.values()))
     glpk = pyglpk.Graph()
     glpk.add_vertices(num_nodes)
     print "Going to begin making %d nodes and %d arcs" % (num_nodes, arcs)
-    nodes = sorted(nodes)
     alias = {node: i + 1 for  i, node in enumerate(nodes)}
+    assert set(demand.keys()).issubset(nodes)
+    total_demand_in = 0
+    total_demand_out = 0
     for node in nodes:
         if demand.get(node, 0):
             glpk.set_demand(alias[node], demand[node])
-    print "Set the node demands"
+            d = demand[node]
+            if d < 0:
+                total_demand_in -= d
+            else:
+                total_demand_out += d
+    print "Set the node demands; in, out = %d, %d" % (total_demand_in, total_demand_out)
+    assert total_demand_in == total_demand_out
     for node, i in alias.items():
-        for target, cost, _ in graph[node]:  # remove .get in final
+        for target, cost in graph[node].items():
             j = alias[target]
+            assert cost >= 0
             glpk.add_edge(i, j)
-            glpk.set_edge_properties((i, j), low=0,
+            glpk.set_edge_properties((i, j),
+                                     low=0,
                                      cap=capacity.get((node, target), 90000),
                                      cost=cost)
-    print "Made GLPK graph object"
+    w = glpk.weak_comp()
+    print "Made GLPK graph object with %d weak components" % w
+    if w > 1:
+        raise ValueError("The graph is not weakly connected!")
     flowdict = glpk.mincost_okalg()[0]
     print "Flowdict len:%d" % len(flowdict)
-    node_ids = {i + 1: node for i, node in enumerate(nodes)}
+    node_ids = {node: i for i, node in alias.items()}
     output = {}
     for i, d in flowdict.items():
         node = node_ids[i]
@@ -259,7 +175,7 @@ def flow_to_csv(flow, filename):
 
     Entries not in the flow vector are left blank.
     """
-    nodes = list(flow)
+    nodes = sorted(list(flow))
     with open(filename, 'wt') as f:
         f.write(',' + ','.join('"%s"' % str(n) for n in nodes))
         f.write('\n')
@@ -269,17 +185,153 @@ def flow_to_csv(flow, filename):
                              for n2 in nodes))
             f.write('\n')
 
-def get_delta_mins(t1, t2):
-    """Return (t2 - t1) in minutes of two timestamps with format HH:MM:SS."""
-    h1, m1, s1 = map(int, t1.split(':'))
-    h2, m2, s2 = map(int, t2.split(':'))
-    return int(60 * (h2 - h1) + m2 - m1 + (s2 - s1) / 60.0)
+class GraphMaker(object):
 
-def minutes_to_str(mins):
-    """Convert the number of minutes since midnight into a string."""
-    h = int(mins / 60)
-    m = mins % 60
-    return "%02d:%02d:00" % (h, m)
+    """
+    An object to help make the Space-Time SEPTA graph.
+
+    :param dbfilename: path to the database file with the tables of links
+                       and stops
+    :param table: name of the table with the links. [The stops table is
+                  assumed to be ``stops``
+    :param overflow: if nonzero, the graph will have "overflow"
+                     edges from the zipcode to the stadium at this cost.
+                     (Default 0)
+
+    The graph it creates takes the form:
+
+        {
+          (node-name, time-object):
+                  {
+                    (node-name, time-object): cost,
+                    ... ,
+                    (node-name, time-object): cost,
+                  }
+        }
+
+    where one node is in the set of another node iff there is a
+    direct link between them, the cost is simply the difference
+    in minutes of their time coordinate.
+    """
+
+    def __init__(self, dbfilename, table, overflow=0):
+        self.table = table
+        self._connection = sqlite3.connect(dbfilename,
+                                        detect_types=sqlite3.PARSE_DECLTYPES)
+        self.cursor = self._connection.cursor()
+        self.overflow = overflow
+        self.stadium_nodes = {}
+        self.zip_nodes = {}
+        self.septa_nodes = {}
+
+    def make_graph(self):
+        """Create the entire graph."""
+        self.load_nodes()
+        self.load_edges()
+        graph = {}
+        graph.update(self.stadium_nodes)
+        graph.update(self.zip_nodes)
+        graph.update(self.septa_nodes)
+        return graph
+
+    def execute(self, cmd, query_args=tuple()):
+        """Shortcut to execute queries."""
+        self.cursor.execute(cmd, query_args)
+
+    def fetchall(self):
+        """Shortcut to cursor.fetchall"""
+        return self.cursor.fetchall()
+
+    def load_nodes(self):
+        """Load all the nodes into their respective attributes."""
+        # First the stadium nodes
+        self.stadium_nodes = {
+            (STADIUM_NODE, self.minutes_to_str(GAMETIME + 5 * i)) : {}
+            for i in xrange(-15, 13)}
+        self.execute(
+            'SELECT dest, arrival FROM %s ' % self.table +
+            'WHERE dest IN (%s)' % ','.join('?' * len(STADIUM_STOPS)),
+            STADIUM_STOPS.keys())
+        for dest, arrival in self.fetchall():
+            time = self.minutes_to_str(self.get_delta_mins(arrival) +
+                                       STADIUM_STOPS[dest])
+            self.stadium_nodes[(STADIUM_NODE, time)] = {}
+        # Now the zipcode nodes -- each node is a **string**
+        self.execute('SELECT DISTINCT zipcode FROM stops')
+        self.zip_nodes = {r[0]: {} for r in self.fetchall()}
+        # Finally the SEPTA nodes
+        self.execute('SELECT DISTINCT origin, departure '
+                     'FROM %s' % self.table)
+        nodes = map(tuple, self.fetchall())
+        self.execute('SELECT DISTINCT dest, arrival '
+                     'FROM %s' % self.table)
+        nodes.extend(map(tuple, self.fetchall()))
+        self.septa_nodes = {n: {} for n in nodes}
+
+    def load_edges(self):
+        """Populate the three dicts with edges."""
+        # First the stadium-stadium edges
+        nodes = sorted(self.stadium_nodes.keys())
+        for n1, n2 in zip(nodes, nodes[1:]):
+            t = self.get_delta_mins(n2[1], n1[1])
+            self.stadium_nodes[n1][n2] = t
+        # now the zipcode-septa edges
+        for zipcode, tgs in self.zip_nodes.items():
+            self.execute('SELECT origin, departure '
+                         'FROM %s ' % self.table +
+                         'INNER JOIN stops '
+                         'ON origin=stop_id '
+                         'WHERE stops.zipcode=?;',
+                         (zipcode,))
+            tgs.update({(o, d): 0 for o, d in self.fetchall()})
+        # now the SEPTA-SEPTA and SEPTA-Stadium edges
+        nodes = sorted(self.septa_nodes.keys())
+        for i, node in enumerate(nodes):
+            self.execute('SELECT dest, arrival, duration ' +
+                         'FROM %s ' % self.table +
+                         'WHERE origin=? AND departure=?',
+                         node)
+            try:
+                dests, arrs, costs = zip(*self.fetchall())
+            except ValueError: # no values
+                continue
+            dapairs = map(tuple, zip(dests, arrs))
+            self.septa_nodes[node].update(zip(dapairs, costs))
+
+            try:  # This entire block is for the waiting links
+                next_stop, time = nodes[i+1]
+            except IndexError:
+                pass
+            else:
+                if next_stop == node[0]:
+                    cost = self.get_delta_mins(time, node[1])
+                    self.septa_nodes[node][nodes[i+1]] =  cost
+
+            if node[0] in STADIUM_STOPS:  # This block is for SEPTA-Stadium
+                cost = STADIUM_STOPS[node[0]]
+                time = self.minutes_to_str(self.get_delta_mins(node[1]) +
+                                           cost)
+                self.septa_nodes[node][(STADIUM_NODE, time)] = cost
+        # finally, the zipcode-stadium edges
+        if self.overflow:
+            first_stadium = min(self.stadium_nodes.keys())
+            for tgts in self.zip_nodes.values():
+                tgts[first_stadium] = self.overflow
+
+    @staticmethod
+    def get_delta_mins(t2, t1="00:00:00"):
+        """Return (t2 - t1) in minutes of two timestamps with format HH:MM:SS."""
+        h1, m1, s1 = map(int, t1.split(':'))
+        h2, m2, s2 = map(int, t2.split(':'))
+        return int(60 * (h2 - h1) + m2 - m1 + (s2 - s1) / 60.0)
+
+    @staticmethod
+    def minutes_to_str(mins):
+        """Convert the number of minutes since midnight into a string."""
+        h = int(mins / 60)
+        m = mins % 60
+        return "%02d:%02d:00" % (h, m)
+
 
 if __name__ == "__main__":
     main()
